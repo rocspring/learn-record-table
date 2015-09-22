47259c53df3aefd45b834d94116f907d970d1251
shangpengwang <shaopengwang@sohu-inc.com>
2015-09-22 22:10:10 +0800

    上传项目框架

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+	less = require('less');
+
+gulp.task('clean', function(){
+	
+});
+
+gulp.task('less', function(){
+
+});
+
+gulp.task('concat', function(){
+
+});
+
+
+gulp.task('bulid', ['clean', 'less', 'concat'], function(){
+	
+})
